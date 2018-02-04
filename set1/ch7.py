c8a1a9b4177d1b07b4ae9f77e4112570f1053f80
Juan Gutierrez <juanny.gee@gmail.com>
2018-02-04 11:26:41 -0500

    Refactor challenge 7
    + test

--- set1/ch7.py
+++ set1/ch7.py
@@ -1,13 +1,29 @@
 import base64
+import sys
 
 from Crypto.Cipher import AES
 
 
+def aes_ecb_128_decrypt(key, ciphertext):
+    return AES.new(key).decrypt(ciphertext)
+
+
 def ch7():
+    """AES-128-ECB decryption wrapper
+
+    >>> from Crypto.Cipher import AES
+    >>> key = "keys" * 4
+    >>> msg = "test" * 4
+    >>> assert msg == aes_ecb_128_decrypt(key, AES.new(key).encrypt(msg))
+    """
     key = "YELLOW SUBMARINE"
     with open("ch7.in") as data_f:
         ciphertext = base64.b64decode(data_f.read().replace('\n', ''))
-        print AES.new(key).decrypt(ciphertext)
+        print aes_ecb_128_decrypt(key, ciphertext)
 
 if __name__ == '__main__':
-    ch7()
+    if '--test' in sys.argv:
+        import doctest
+        doctest.testmod()
+    elif '--solve' in sys.argv:
+        ch7()
